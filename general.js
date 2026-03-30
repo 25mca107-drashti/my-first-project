46e9a5871dee47d788ea58ddd6cabc27d2964d6f
25mca107-drashti <25mca107@charusat.edu.in>
2026-03-30 19:10:09 +0530

    Update general.js

--- general.js
+++ general.js
@@ -2,24 +2,47 @@ const axios = require('axios');
 
 // Get all books
 async function getAllBooks() {
-  const response = await axios.get("http://localhost:3000/books");
-  return response.data;
+  try {
+    const response = await axios.get("http://localhost:3000/books");
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Get by ISBN
 async function getBookByISBN(isbn) {
-  const response = await axios.get(`http://localhost:3000/isbn/${isbn}`);
-  return response.data;
+  try {
+    const response = await axios.get(`http://localhost:3000/isbn/${isbn}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Get by Author
 async function getBookByAuthor(author) {
-  const response = await axios.get(`http://localhost:3000/author/${author}`);
-  return response.data;
+  try {
+    const response = await axios.get(`http://localhost:3000/author/${author}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Get by Title
 async function getBookByTitle(title) {
-  const response = await axios.get(`http://localhost:3000/title/${title}`);
-  return response.data;
+  try {
+    const response = await axios.get(`http://localhost:3000/title/${title}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+module.exports = {
+  getAllBooks,
+  getBookByISBN,
+  getBookByAuthor,
+  getBookByTitle
+};
